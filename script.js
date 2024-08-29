ac9de8f439d9a72ef59de92dcd601545125e325e
Andrea Gulli <andreagulli11@gmail.com>
2024-08-30 00:19:00 +0200

    added video trailer on thumbnails img hover

---
+++ script.js
@@ -0,0 +1,33 @@
+const videoContainer = document.getElementById('video')
+const imgContent = document.querySelector('.top-img-content')
+const gtaImg = document.getElementById('gtaImg')
+// Aggiunta video trailer dinamico sull'hover dell'img GTA VI
+
+videoContainer.addEventListener('mouseover', () =>{
+    gtaImg.classList.add('hidden') 
+    iframe.classList.remove('hidden')
+    iframe.src = "https://www.youtube.com/embed/QdBZY2fkU-0?si=C72q8dFt2w-vPhUX&autoplay=1";
+    iframe.classList.add('active')
+})
+
+// Creo il tag per contenere il video e la source 
+let iframe = document.createElement('iframe')
+iframe.width = "560";
+iframe.height = "315";
+iframe.src = "https://www.youtube.com/embed/QdBZY2fkU-0?si=C72q8dFt2w-vPhUX";
+iframe.title = "YouTube video player";
+iframe.style.borderRadius = "15px"
+iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+iframe.referrerPolicy = "strict-origin-when-cross-origin";
+iframe.allowFullscreen = true; 
+
+// Aggiungi il listener per l'evento 'mouseout'
+videoContainer.addEventListener('mouseout', () => {
+    gtaImg.classList.remove('hidden'); 
+    iframe.classList.add('hidden')
+    iframe.src = ""
+    iframe.classList.remove('active')
+});
+
+videoContainer.appendChild(iframe)
+
