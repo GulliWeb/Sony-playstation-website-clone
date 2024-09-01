29179636fbe88aba6919dea8a556a4cbca201167
Andrea Gulli <andreagulli11@gmail.com>
2024-09-01 23:45:37 +0200

    add section carousel (static)

--- script.js
+++ script.js
@@ -28,5 +28,27 @@ videoContainer.addEventListener('mouseout', () => {
     iframe.src = ""
     iframe.classList.remove('active')
 });
-
 videoContainer.appendChild(iframe)
+
+// CAROUSEL
+const carousel = document.getElementById('carousel')
+const card = ['card-1', 'card-2', 'card-3', 'card-4', 'card-5', 'card-6']
+const carouselSources = ['https://gmedia.playstation.com/is/image/SIEPDC/astro-bot-keyart-01-en-23may24?$1200px$', 'https://gmedia.playstation.com/is/image/SIEPDC/star-wars-outlaws-keyart-01-en-17jul23?$1200px$', 'https://gmedia.playstation.com/is/image/SIEPDC/call-of-duty-black-ops-6-open-beta-keyart-01-en-28aug24?$1200px$', 'https://gmedia.playstation.com/is/image/SIEPDC/indiana-jones-and-the-great-circle-keyart-01-en-21aug24?$1200px$', 'https://gmedia.playstation.com/is/image/SIEPDC/fc-25-keyart-01-04jul24$en?$1200px$', 'https://gmedia.playstation.com/is/image/SIEPDC/valorant-keyart-01-en-31may24?$1200px$']
+let carouselImg = [] 
+
+// Creo dinamicamente le card
+card.forEach((item, index) => {
+    const cardContainer = document.createElement('div')
+    cardContainer.classList.add('thumb-card', item)
+    carousel.style.gap = ('12px')
+    carousel.appendChild(cardContainer)
+    // Creo figure
+    const carouselFigure = document.createElement('figure')
+    carouselFigure.style.borderRadius = ('12px')
+    cardContainer.appendChild(carouselFigure)
+
+    // Creo Img
+    carouselImg = document.createElement('img')
+    carouselImg.src = (carouselSources[index])
+    carouselFigure.appendChild(carouselImg)
+});
